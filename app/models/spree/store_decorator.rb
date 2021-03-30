8a3d742b5c429f6f42bfbd1bc610f9aabef70a01
Marko Bošković <bosskovic@gmail.com>
2021-03-30 12:02:23 +0200

    updates store decorator

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -1,3 +1,5 @@
-Spree::Store.class_eval do
+module Spree::StoreDecorator
   has_many :additional_store_settings, dependent: :destroy, class_name: "Spree::AdditionalStoreSetting"
+
+  Spree::Store.prepend.self
 end
