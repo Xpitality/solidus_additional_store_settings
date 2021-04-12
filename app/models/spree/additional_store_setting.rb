bdfd5c6f7c2aa9a065367b0b13dd5c977346ee2b
Marko Bošković <bosskovic@gmail.com>
2021-04-12 20:54:11 +0200

    adds find_by_label

--- app/models/spree/additional_store_setting.rb
+++ app/models/spree/additional_store_setting.rb
@@ -2,5 +2,11 @@ module Spree
   class AdditionalStoreSetting < ActiveRecord::Base
     belongs_to :store, class_name: 'Spree::Store'
     validates :label, presence: true, uniqueness: {scope: :store_id}
+
+    class << self
+      def find_by_label(label)
+        where(label: label).first
+      end
+    end
   end
 end
